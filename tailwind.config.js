5560c20640dcff43660ea6638b408fa48553664e
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-04-06 14:44:36 -0300

    feat: add two fonts and color pallete to tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Title: ["Share Tech Mono", "monospace"],
+        Source: ["Source Code Pro", "monospace"],
+      },
+      colors: {
+        darkBlack: "#1D1D1D",
+        lightBlack: "#27272A",
+        white: "#D9D9D9",
+        darkGray: "#52525B",
+        lightGray: "#A1A1AA",
+        lightOrange: "#CE8669",
+        darkOrange: "#FF7038",
+      },
+    },
   },
   plugins: [],
-}
+};
