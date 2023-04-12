37683aed441b17afd843c28472e0bc202c7432ac
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-04-12 11:50:12 -0300

    style: edit menu padding size

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -14,7 +14,7 @@ const Menu = ({ locales }: Language) => {
   const { theme } = useTheme();
 
   return (
-    <div className="flex items-end justify-between w-full px-10 py-8 fixed bg-white dark:bg-lightBlack bg-opacity-50 backdrop-blur-sm dark:bg-opacity-50">
+    <div className="flex items-end justify-between w-full px-10 py-3 fixed bg-white dark:bg-lightBlack bg-opacity-50 backdrop-blur-sm dark:bg-opacity-50">
       <Image alt="" src={theme === "dark" ? logo : logoWhite} width={80} />
       <div className="flex">
         <ToggleLanguage locales={locales} />
