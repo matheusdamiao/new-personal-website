a4925f45530243609c41518ba67591d150eca614
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-04-18 20:11:31 -0300

    feat: add country flags to toggle language component

--- components/ToggleLanguage.tsx
+++ components/ToggleLanguage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import brazil from "./../assets/icons/brazil.svg";
+import usa from "./../assets/icons/usa.svg";
 
 type Language = {
   locales: string[] | undefined;
@@ -12,18 +15,43 @@ const ToggleLanguage = ({ locales }: Language): JSX.Element => {
     return push("/", undefined, { locale: lang });
   };
   return (
+    // <div className="">
+    //   {locales?.map((lang) => {
+    //     return (
+    //       <button
+    //         key={lang}
+    //         className="p-2 uppercase"
+    //         onClick={() => changeLocale(lang)}
+    //       >
+    //         {lang === "pt" ? (
+    //           <Image src={brazil} alt="" width={25} />
+    //         ) : (
+    //           <Image src={usa} alt="" width={25} />
+    //         )}
+    //       </button>
+    //     );
+    //   })}
+    // </div>
+
     <div className="">
-      {locales?.map((lang) => {
-        return (
-          <button
-            key={lang}
-            className="p-2 uppercase"
-            onClick={() => changeLocale(lang)}
-          >
-            {lang}
-          </button>
-        );
-      })}
+      {locales
+        ?.filter((lang) => lang !== locale)
+        .map((l) => {
+          return (
+            <button
+              key={l}
+              className="p-2 uppercase"
+              onClick={() => changeLocale(l)}
+            >
+              <Image
+                src={l === "pt" ? brazil : usa}
+                alt=""
+                width={25}
+                height={25}
+              />
+            </button>
+          );
+        })}
     </div>
   );
 };
