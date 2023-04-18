f1c77db8f4f31f4af9678182b5025b0e9c08fcec
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-04-18 12:27:08 -0300

    feat: add new logo

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import ToggleLanguage from "./ToggleLanguage";
 import ToggleTheme from "./ToggleTheme";
-import logo from "./../assets/images/logo_md.svg";
-import logoWhite from "./../assets/images/logoWhite.svg";
+// import logo from "./../assets/images/logo_md.svg";
+// import logoWhite from "./../assets/images/logoWhite.svg";
+import logo from "./../assets/icons/logo-brand-dark.svg";
+import logoWhite from "./../assets/icons/logo-brand-light.svg";
+
 import Image from "next/image";
 import { useTheme } from "next-themes";
 
@@ -15,7 +18,7 @@ const Menu = ({ locales }: Language) => {
 
   return (
     <div className="flex items-end z-50 justify-between w-full px-10 py-3 fixed bg-white dark:bg-lightBlack bg-opacity-50 backdrop-blur-sm dark:bg-opacity-50">
-      <Image alt="" src={theme === "dark" ? logo : logoWhite} width={80} />
+      <Image alt="" src={theme === "dark" ? logoWhite : logo} width={80} />
       <div className="flex">
         <ToggleLanguage locales={locales} />
         <ToggleTheme />
