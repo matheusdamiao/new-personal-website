d4f5afd288adfbf19630fae7677b27d9b1bdb70d
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-04-25 20:41:28 -0300

    style: add countries flag and update opacity according to its url locale

--- components/ToggleLanguage.tsx
+++ components/ToggleLanguage.tsx
@@ -11,49 +11,59 @@ type Language = {
 const ToggleLanguage = ({ locales }: Language): JSX.Element => {
   const { locale, push } = useRouter();
 
+  const buttonUsa = {
+    opacity: `${locale === "en" ? "1" : "0.2"}`,
+  };
+
+  const buttonBrazil = {
+    opacity: `${locale === "pt" ? "1" : "0.2"}`,
+  };
+
+  console.log(locale);
+
   const changeLocale = (lang: string): Promise<boolean> => {
     return push("/", undefined, { locale: lang });
   };
   return (
-    // <div className="">
-    //   {locales?.map((lang) => {
-    //     return (
-    //       <button
-    //         key={lang}
-    //         className="p-2 uppercase"
-    //         onClick={() => changeLocale(lang)}
-    //       >
-    //         {lang === "pt" ? (
-    //           <Image src={brazil} alt="" width={25} />
-    //         ) : (
-    //           <Image src={usa} alt="" width={25} />
-    //         )}
-    //       </button>
-    //     );
-    //   })}
-    // </div>
-
-    <div className="z-20">
-      {locales
-        ?.filter((lang) => lang !== locale)
-        .map((l) => {
-          return (
-            <button
-              key={l}
-              className="p-2 uppercase"
-              onClick={() => changeLocale(l)}
-            >
-              {/* <Image
-                src={l === "pt" ? brazil : usa}
-                alt=""
-                width={25}
-                height={25}
-              /> */}
-              {l === "pt" ? "PT" : "EN"}
-            </button>
-          );
-        })}
+    <div className="flex justify-center items-center">
+      {locales?.map((lang) => {
+        return (
+          <button
+            key={lang}
+            className="p-2 uppercase"
+            onClick={() => changeLocale(lang)}
+          >
+            {lang === "pt" ? (
+              <Image src={brazil} style={buttonBrazil} alt="" width={20} />
+            ) : (
+              <Image src={usa} alt="" style={buttonUsa} width={20} />
+            )}
+          </button>
+        );
+      })}
     </div>
+
+    // <div className="z-10">
+    //   {locales
+    //     ?.filter((lang) => lang !== locale)
+    //     .map((l) => {
+    //       return (
+    //         <button
+    //           key={l}
+    //           className="p-2 uppercase"
+    //           onClick={() => changeLocale(l)}
+    //         >
+    //           {/* <Image
+    //             src={l === "pt" ? brazil : usa}
+    //             alt=""
+    //             width={25}
+    //             height={25}
+    //           /> */}
+    //           {l === "pt" ? "PT" : "EN"}
+    //         </button>
+    //       );
+    //     })}
+    // </div>
   );
 };
 
